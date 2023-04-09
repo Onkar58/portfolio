3e63755f301ea4b13b45cdbd90f5b3e3d51c4823
Onkar Waghmode <80036030+Onkar58@users.noreply.github.com>
2023-04-09 23:59:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import About from "./elements/homepage/About";
+import About from "./elements/homepage/about";
 import Contact from "./elements/homepage/Contact";
-import Homepage from "./elements/homepage/Homepage";
+import Homepage from "./elements/homepage/homepage";
 import Projects from "./elements/homepage/projects";
 import Navbar from "./elements/navbar/navbar"
 // import { NavLink } from "react-router-dom";
